47ee417857a5f1f9c7953cb6241fd0afc3fe8944
Yutong Hu <yutong.hu@pursuit.org>
2025-12-15 16:29:03 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -116,6 +116,8 @@ app.get('/auth/google', (req, res) => {
   const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/gmail.modify',
       'https://www.googleapis.com/auth/gmail.labels'
     ],
